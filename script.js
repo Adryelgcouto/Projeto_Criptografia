93ae84bc9bd412a2361447e3a4a4cba035b36692
Adryel Couto <adryelcouto@Laptop-de-Adryel.local>
2024-01-25 23:24:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+function disbledButton() {
+  let textoInput = document.querySelector("input").value;
+  let buttonCriptografia = document.querySelector(".criptografar");
+  let buttonDescriptografia = document.querySelector(".descriptografar");
+  if (textoInput.length > 0) {
+    buttonCriptografia.removeAttribute("disabled");
+    buttonDescriptografia.removeAttribute("disabled");
+  } else {
+    buttonCriptografia.setAttribute("disabled", "true");
+    buttonDescriptografia.setAttribute("disabled", "true");
+    let divParaOtexto = document.querySelector(".criptografado");
+    divParaOtexto.innerHTML = `
+    <img src="/assents/img/High quality products 1 1.svg" alt="" />
+    <h3>Nenhuma mensagem encontrada</h3>
+    <p>Digite um texto que você deseja criptografar ou descriptografar.</p>`;
+  }
+}
+function valorCriptografar() {
+  let textoParaCriptografar = document.querySelector("input").value;
+  let texto = textoParaCriptografar;
+  texto = textoParaCriptografar
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("a", "ai")
+    .replaceAll("o", "ober")
+    .replaceAll("u", "ufat");
+  let divParaOtexto = document.querySelector(".criptografado");
+  divParaOtexto.innerHTML = `<p class='criptP'>${texto}</p>
+  <button onclick="copiar()" >COPIAR</button>
+  `;
+}
+
+function copiar() {
+  let texteCop = document.querySelector(".criptP");
+  let textecopiar = texteCop.textContent;
+  navigator.clipboard
+    .writeText(textecopiar)
+    .then(() => {
+      alert("Texto copiado para a área de transferência: " + textecopiar);
+    })
+    .catch((err) => {
+      console.error("Erro ao copiar texto: ", err);
+    });
+}
+
+function descriptografar() {
+  let textoParaDescriptografar = document.querySelector("input").value;
+  let textoDescriptografado = textoParaDescriptografar
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ai", "a")
+    .replaceAll("ober", "o")
+    .replaceAll("ufat", "u");
+  let divParaOtexto = document.querySelector(".criptografado");
+  divParaOtexto.innerHTML = `<p class='criptP'>${textoDescriptografado}</p>
+  <button onclick="copiar()" >COPIAR</button>
+  `;
+}
