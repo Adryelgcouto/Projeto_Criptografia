9fe1c6e72e311fd33382d88c7c393cbd614b814b
Adryel Couto <adryelcouto@Laptop-de-Adryel.local>
2024-01-26 00:27:58 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+let arrayAcento = [
+  "Á",
+  "Â",
+  "Ã",
+  "À",
+  "É",
+  "Ê",
+  "Í",
+  "Î",
+  "Ó",
+  "Ô",
+  "Õ",
+  "Ú",
+  "Û",
+  "Ç",
+  "á",
+  "â",
+  "ã",
+  "à",
+  "é",
+  "ê",
+  "í",
+  "î",
+  "ó",
+  "ô",
+  "õ",
+  "ú",
+  "û",
+  "ç",
+];
+function acentoMausculas() {
+  let textoInput = document.querySelector("input").value;
+  for (let i = 0; i < arrayAcento.length; i++) {
+    let letra = arrayAcento[i];
+    if (textoInput.includes(letra)) {
+      let divParaOtexto = document.querySelector(".criptografado");
+      divParaOtexto.innerHTML =
+        '<h1 class="notPode">Não pode palavras com acento e nem maiúsculas!</h1>';
+    }
+  }
+  for (let i = 0; i < textoInput.length; i++) {
+    if (textoInput[i] >= "A" && textoInput[i] <= "Z") {
+      let divParaOtexto = document.querySelector(".criptografado");
+      divParaOtexto.innerHTML =
+        '<h1 class="notPode">Não pode palavras com acento e nem maiúsculas!</h1>';
+    }
+  }
+}
+
 function disbledButton() {
   let textoInput = document.querySelector("input").value;
   let buttonCriptografia = document.querySelector(".criptografar");
@@ -28,19 +77,7 @@ function valorCriptografar() {
   divParaOtexto.innerHTML = `<p class='criptP'>${texto}</p>
   <button onclick="copiar()" >COPIAR</button>
   `;
-}
-
-function copiar() {
-  let texteCop = document.querySelector(".criptP");
-  let textecopiar = texteCop.textContent;
-  navigator.clipboard
-    .writeText(textecopiar)
-    .then(() => {
-      alert("Texto copiado para a área de transferência: " + textecopiar);
-    })
-    .catch((err) => {
-      console.error("Erro ao copiar texto: ", err);
-    });
+  acentoMausculas();
 }
 
 function descriptografar() {
@@ -55,4 +92,16 @@ function descriptografar() {
   divParaOtexto.innerHTML = `<p class='criptP'>${textoDescriptografado}</p>
   <button onclick="copiar()" >COPIAR</button>
   `;
+}
+function copiar() {
+  let texteCop = document.querySelector(".criptP");
+  let textecopiar = texteCop.textContent;
+  navigator.clipboard
+    .writeText(textecopiar)
+    .then(() => {
+      alert("Texto copiado para a área de transferência: " + textecopiar);
+    })
+    .catch((err) => {
+      console.error("Erro ao copiar texto: ", err);
+    });
 }
